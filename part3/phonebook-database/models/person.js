64a13ad8fd8f70b30298f6b79bd285d1272eaf7d
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2025-03-29 12:51:54 -0400

    Configure lint in phonebook-database

--- part3/phonebook-database/models/person.js
+++ part3/phonebook-database/models/person.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
-const url = process.env.MONGODB_URI;
-console.log('connecting to', url);
+mongoose.set('strictQuery', false)
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => console.log('connected to MongoDB'))
-  .catch((error) => console.log('error connecting to MongoDB:', error.message));
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message))
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
@@ -22,14 +22,14 @@ const personSchema = new mongoose.Schema({
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
